241a5dfc5a2f750a94ac81690b39b807d03c7fac
congminh869 <69363249+congminh869@users.noreply.github.com>
2021-11-25 14:37:44 +0700

    Update sweep0.py

--- sweep0.py
+++ sweep0.py
@@ -1,101 +1,84 @@
-from gpiozero import Servo
-import math
-import time
+import serial
+from time import sleep
 
-from gpiozero.pins.pigpio import PiGPIOFactory
-
-factory = PiGPIOFactory()
-
-servo = Servo(13, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
-
-servo.value = math.sin(math.radians(45))
-
-degree_left = degree_right = 0
-degree_up = degree_down = 0
-
-g_start = 0
-left = 1
-right = 2
-up = 3
-down = 4
-left_up = 5
-left_down = 6
-right_up = 7
-right_down = 8
-g_stop = 9
-
-g_start_lr = 0
-g_start_ud = 0
-
-G_FACTORY = 2
-RATIO = float(5/90)
-START_DEGREE = 45*RATIO + G_FACTORY
-DEGREE_NOW_LR = START_DEGREE
-DEGREE_NOW_UD = START_DEGREE
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 def ratio_start():
-    servo.value = math.sin(math.radians(START_DEGREE))
+        print('ratio_start')
 
 def ratio_left():
-    #degree_left++;
-    DEGREE_NOW_LR = DEGREE_NOW_LR - 1
-    servo.value = math.sin(math.radians(DEGREE_NOW_LR))
+        print('ratio_left - 1')
+        ser.write(b'1')
+        ser.flush()
 
 def ratio_right():
-    #degree_right++;
-    DEGREE_NOW_LR = DEGREE_NOW_LR + 1
-    servo.value = math.sin(math.radians(DEGREE_NOW_LR))
+        print('ratio_right - 2')
+        ser.write(b'2')
+        ser.flush()
 
 def ratio_up():
-    #degree_up++;
-    DEGREE_NOW_UD = DEGREE_NOW_UD + 1
-    servo.value = math.sin(math.radians(DEGREE_NOW_UD))
+        print('ratio_up - 3')
+        ser.write(b'3')
+        ser.flush()
 
 def ratio_down():
-    #degree_down++;
-    DEGREE_NOW_UD = DEGREE_NOW_UD - 1
-    servo.value = math.sin(math.radians(DEGREE_NOW_UD))
+        print('ratio_down - 4')
+        ser.write(b'4')
+        ser.flush()
 
 def ratio_left_up():
-    ratio_left()
-    ratio_up()
+        print('ratio_left_up  - 5')
+        ser.write(b'5')
+        ser.flush()
 
 def ratio_left_down():
-    ratio_left()
-    ratio_down()
+        print('ratio_left_down - 6')
+        ser.write(b'6')
+        ser.flush()
 
 def ratio_right_up():
-    ratio_right()
-    ratio_up()
+        print('ratio_right_up - 7')
+        ser.write(b'7')
+        ser.flush()
 
 def ratio_right_down():
-    ratio_right()
-    ratio_down()
+        print('ratio_right_down - 8')
+        ser.write(b'8')
+        ser.flush()
 
 def ratio_stop():
-    #degree_left = degree_right = degree_up = degree_down = 0
-    #degree_left_up = degree_left_down = degree_right_up = degree_right_down = 0
-    servo.value = None
+        print('ratio_stop - 9')
+        ser.write(b'9')
+        ser.flush()
 
 def ratio(command):
-    switcher={
-        0:ratio_start,
-        1:ratio_left,
-        2:ratio_right,
-        3:ratio_up,
-        4:ratio_down,
-        5:ratio_left_up,
-        6:ratio_left_down,
-        7:ratio_right_up,
-        8:ratio_right_down,
-        9:ratio_stop
-        }
-    return switcher.get(command, "Invalid status_ratio")
-
+        if command == 0:
+                ratio_start()
+        elif command == 1:
+                ratio_left()
+        elif command == 2:
+                ratio_right()
+        elif command == 3:
+                ratio_up()
+        elif command == 4:
+                ratio_down()
+        elif command == 5:
+                ratio_left_up()
+        elif command == 6:
+                ratio_left_down()
+        elif command == 7:
+                ratio_right_up()
+        elif command == 8:
+                ratio_right_down()
+        elif command == 9:
+                ratio_stop()
+        
 # while True:
-#     ratio(0)
-#     while True:
 #         ratio(1)
-    #for i in range(0, 120):
-        #servo.value = math.sin(math.radians(i))
-        #time.sleep(0.1)
+# for i in range(0,10):
+#         print(f'ratio({str(i)}) = ', ratio(i))
+# try:
+#         while True:
+#               #truyen lenh
+# except KeyboardInterrupt:
+#         print("closing...")
